ae3aea58a14c60c83a9fc396448042a5acc5864f
Hiroya Yoshino <108524504+hiroya->
2022-07-13 21:26:32 +0000

    [Add] 4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = User.find(params[:id])
+    @books = @users.books
   end
   
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/edit'
-  get 'users/show'
+
+  root to: "homes#top"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root to: "homes#top"
+  resources :books, only: [:new, :index, :create, :show, :destroy]
+  resources :user, only: [:show, :edit, :update]
   
 end
