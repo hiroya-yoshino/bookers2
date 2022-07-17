3d9b020ac2366ae49b6cef6074cd7ef85e7f5dcd
Hiroya Yoshino <108524504+hiroya->
2022-07-17 12:31:29 +0000

    [Add] 4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,16 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
+  
+  def index
+    @user = current_user
+    @book = Book.new 
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   private
   
   def user_params
-    params.require(:user).permit(:name)
+    params.require(:user).permit(:name, :image, :introduction)
   end
 end
