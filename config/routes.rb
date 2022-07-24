7acd7f2d62f85d3e5b70294076e7ef75179ddf93
Hiroya Yoshino <108524504+hiroya->
2022-07-24 10:56:15 +0000

    [Finish] 4-25

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'home/about' => 'homes#about'
   devise_for :users
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :books, only: [:new, :index, :create, :show, :destroy, :edit, :update, :destroy]
